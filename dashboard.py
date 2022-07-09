d842bdcfc4cd9aab644c7982ba547f400836d30e
szczerski <szczerski@gmail.com>
2022-07-09 20:12:34 +0200

    sql: most common tasks and apps

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,20 @@ def getFromDb(sql):
     )
 
 
+# top 5 most used apps today
 sql = "SELECT name, title, time FROM journal WHERE date(datetime(timestamp , 'unixepoch')) = date('now') "
 todayByName = getFromDb(sql)
-print(todayByName.groupby("name").sum())
+todayByName["time"] = pd.to_timedelta(todayByName["time"], unit="s")
+print(todayByName[["name", "time"]].groupby("name").sum()[:5])
+
+# top 5 most used apps all time
+sql = "SELECT name, time FROM journal"
+todayByName = getFromDb(sql)
+todayByName["time"] = pd.to_timedelta(todayByName["time"], unit="s")
+print(todayByName.groupby("name").sum()[:5])
+
+# top 5 most task spent on today
+sql = "SELECT title, time FROM journal"
+todayByName = getFromDb(sql)
+todayByName["time"] = pd.to_timedelta(todayByName["time"], unit="s")
+print(todayByName.groupby("title").sum()[:5])
