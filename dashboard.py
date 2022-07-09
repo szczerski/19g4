ceb3e8c339a95c1893faf1ba76a66053acea3a32
szczerski <szczerski@gmail.com>
2022-07-09 19:26:38 +0200

    add pandas

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,14 @@
 import sqlite3
+import pandas as pd
 
 
-con = sqlite3.connect("19g4.db")
-cur = con.cursor()
-cur.execute(
-    "SELECT name, title, time FROM journal WHERE date(datetime(timestamp , 'unixepoch')) = date('now') ",
-)
-data = cur.fetchall()
-for i in data:
-    print(f"{i[0]} {i[1]} : {i[2]} seconds")
-    
-con.commit()
-con.close()
+def getFromDb(sql):
+    return pd.read_sql_query(
+        sql,
+        sqlite3.connect("19g4.db"),
+    )
+
+
+sql = "SELECT name, title, time FROM journal WHERE date(datetime(timestamp , 'unixepoch')) = date('now') "
+todayByName = getFromDb(sql)
+print(todayByName.groupby("name").sum())
