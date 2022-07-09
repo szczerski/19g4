c5425b5d4d3a80afa4ff34129c74a9c63088a371
szczerski <szczerski@gmail.com>
2022-07-09 21:49:08 +0200

    add clipboard table

--- create_db.py
+++ create_db.py
@@ -5,5 +5,9 @@ cur = con.cursor()
 cur.execute(
     "CREATE TABLE IF NOT EXISTS journal (id INTEGER PRIMARY KEY, timestamp INTEGER, name TEXT, title TEXT, time INTEGER)"
 )
+
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS clipboard (id INTEGER PRIMARY KEY, id_journal INTEGER, timestamp INTEGER, cb TEXT)"
+)
 con.commit()
 con.close()
