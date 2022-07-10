4473f6f0db068a142503757b40a59d6f434ca4ac
szczerski <szczerski@gmail.com>
2022-07-10 11:37:27 +0200

    add media windows

--- spy.py
+++ spy.py
@@ -1,7 +1,11 @@
+import asyncio
 import psutil, win32process, win32gui
 import time
 import sqlite3
 import pyperclip
+from winrt.windows.media.control import (
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager,
+)
 
 
 def getForegroundWindowName():
@@ -53,10 +57,33 @@ def getClipboard():
     return clipboard
 
 
+async def getMediaInfo():
+    sessions = await MediaManager.request_async()
+
+    current_session = sessions.get_current_session()
+
+    if current_session:  # there needs to be a media session running
+        if current_session.source_app_user_model_id == "chrome.exe":
+            info = await current_session.try_get_media_properties_async()
+
+            # song_attr[0] != '_' ignores system attributes
+            info_dict = {
+                song_attr: info.__getattribute__(song_attr)
+                for song_attr in dir(info)
+                if song_attr[0] != "_"
+            }
+
+            info_dict["genres"] = list(info_dict["genres"])
+            return info_dict["artist"], info_dict["title"]
+
+
 time_seconds = time.time()
 current_window_title = getForegroundWindowTitle()
 current_window_name = getForegroundWindowName()
-cb_last = ''
+cb_last = ""
+media_artist_last = ""
+media_title_last = ""
+
 
 while True:
 
@@ -77,3 +104,15 @@ while True:
         id_journal = getDbJournalId()
         pushToDbClipboard(cb, id_journal)
         cb_last = cb
+
+    media_artist, media_title = asyncio.run(getMediaInfo())
+    if (
+        media_artist != ""
+        and media_title != ""
+        and media_artist != media_artist_last
+        and media_title != media_title_last
+    ):
+        print("MEDIA: ", media_title, media_artist)
+
+        media_title_last = media_title
+        media_artist_last = media_artist
